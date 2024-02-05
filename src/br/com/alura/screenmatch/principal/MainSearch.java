83a63357d207854fb01b7b7a44d62474d7f9c7ec
RobertoTadeuAlmeida <robertotadeudealmeida@gmail.com>
2024-02-05 15:54:59 -0300

    API
    Começando a integração da API OMDb.

---
+++ src/br/com/alura/screenmatch/principal/MainSearch.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MainSearch {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=f8834d4b"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
+
