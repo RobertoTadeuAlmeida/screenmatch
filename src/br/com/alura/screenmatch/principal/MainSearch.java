24f79b365ea363f6941c791cb409f1c50e9f99b2
RobertoTadeuAlmeida <robertotadeudealmeida@gmail.com>
2024-02-16 11:38:36 -0300

    Main Search
    implementei um mecanismo simples de busca + API no projeto

--- src/br/com/alura/screenmatch/principal/MainSearch.java
+++ src/br/com/alura/screenmatch/principal/MainSearch.java
@@ -5,13 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainSearch {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        String key = "f8834d4b";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual filme você busca?");
+        var search = scanner.nextLine();
+        String endereco = "https://www.omdbapi.com/?t=" + search + "&apikey="+key;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=f8834d4b"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
